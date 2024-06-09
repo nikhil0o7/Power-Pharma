723da8e1d8b8569464c1d4ffe41d38a591c68b49
nikhil0o7 <nikhil888.srirangam@gmail.com>
2024-06-09 11:09:03 -0400

    minor fix

--- next.config.js
+++ next.config.js
@@ -5,8 +5,8 @@ const nextConfig = {
     // if you'd like to see the remotePatterns version, I'm happy to merge a PR
     domains: [
       "localhost",
+      "power-pharma.vercel.app",
       "powerpharma.up.railway.app",
-      "power-pharma.vercel.app/"
     ],
   },
 };
