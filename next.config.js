7bca9ff696702aaeb0077d88c1b4649458fa6665
nikhil0o7 <nikhil888.srirangam@gmail.com>
2024-06-09 10:44:48 -0400

    added a new domain

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
     // if you'd like to see the remotePatterns version, I'm happy to merge a PR
     domains: [
       "localhost",
-      "powerpharma.up.railway.app",
+      "https://power-pharma.vercel.app/"
     ],
   },
 };
